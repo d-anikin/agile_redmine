0077b9a6ed8509467a926442a59fb3839297712f
Dmitry A <mr.ads@yandex.ru>
2016-05-19 10:09:37 +0300

    fix link && change icons

--- init.rb
+++ init.rb
@@ -47,7 +47,7 @@ Redmine::Plugin.register :redmine_agile do
   menu :top_menu, :all_issues, { :controller => 'issues', :action => 'index', :set_filter => 1}, :caption => :label_issue_view_all
   menu :top_menu, :issues_backlog, { :controller => 'issues', :action => 'index', :set_filter => 1, :f => ['status_id', 'fixed_version_id', 'version_status'], :op => { :status_id => 'o', :fixed_version_id => '!*', :version_status => 'open' }, :group_by => 'project', :per_page => 100}, :caption => :label_backlog
   menu :top_menu, :agile_boards, { :controller => 'agile_boards', :action => 'index', :set_filter => 1, :f => ['status_id', 'fixed_version_id', 'version_status'], :op => { :status_id => 'o', :fixed_version_id => '*', :version_status => 'open' } }, :caption => :label_agile
-  menu :top_menu, :issues_gantt, { :controller => 'gantt', :action => 'show', :set_filter => 1, :f => ['status_id', 'fixed_version_id', 'version_status'], :op => { :status_id => 'o', :fixed_version_id => '*', :version_status => 'open' } }, :caption => :label_gantt
+  menu :top_menu, :issues_gantt, { :controller => 'gantts', :action => 'show', :set_filter => 1, :f => ['status_id', 'fixed_version_id', 'version_status'], :op => { :status_id => 'o', :fixed_version_id => '*', :version_status => 'open' } }, :caption => :label_gantt
 
   project_module :agile do
     permission :manage_public_agile_queries, {:agile_queries => [:new, :create, :edit, :update, :destroy]}, :require => :member
